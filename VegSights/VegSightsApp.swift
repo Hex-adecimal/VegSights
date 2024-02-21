94afe0d1a6748a8b7f4e3f9cfe24f0f547c90bc2
Hex-adecimal <lu.penza@studenti.unina.it>
2024-02-21 11:10:56 +0100

    Initial Commit

---
+++ VegSights/VegSightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VegSightsApp.swift
+//  VegSights
+//
+//  Created by Luigi Penza on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct VegSightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
